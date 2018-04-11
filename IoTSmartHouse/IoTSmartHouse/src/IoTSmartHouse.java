8cdac38f89ea3141a614fbf4823af66810c49f3f
matthew-pham <matthewpham55@gmail.com>
2018-04-10 23:17:36 -0700

    Initial Commit

--- IoTSmartHouse/IoTSmartHouse/src/IoTSmartHouse.java
+++ IoTSmartHouse/IoTSmartHouse/src/IoTSmartHouse.java
@@ -10,12 +10,12 @@ public class IoTSmartHouse {
 	public static void main(String[] args)throws NoSuchAlgorithmException{
 		Mongo connect = new Mongo();
 		connect.init(); 
-		//AccountManager create = new AccountManager();
-		//create.createAccount("matt", "matt");
+		AccountManager create = new AccountManager();
+		create.createAccount("nate", "nate");
 		//create.changePassword("matt","matthew","bob");
 		
-		Authentication authenticate = new Authentication();
-		authenticate.authenticate("matt", "bob");
+		//Authentication authenticate = new Authentication();
+		//authenticate.authenticate("matt", "bob");
 
 	}
 	
